012722745003260c9edfcc50dd5b274d43289a4c
tom <tom7737@sina.com>
2018-01-03 11:37:13 +0800

    完善加密文件上传功能

--- commons/src/main/java/com/gt/ms/ueditor/upload/BinaryUploader.java
+++ commons/src/main/java/com/gt/ms/ueditor/upload/BinaryUploader.java
@@ -7,6 +7,7 @@ import com.gt.ms.ueditor.define.State;
 import com.gt.ms.ueditor.define.BaseState;
 import com.gt.ms.ueditor.define.FileType;
 import com.gt.ms.ueditor.manager.IUeditorFileManager;
+import com.gt.ms.ueditor.util.EncAndDecConstants;
 import com.gt.ms.utils.IOUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -18,53 +19,61 @@ import java.util.List;
 
 public class BinaryUploader {
 
-	public static final State save(IUeditorFileManager fileManager, HttpServletRequest request,
+    public static final State save(IUeditorFileManager fileManager, HttpServletRequest request,
                                    ActionConfig conf) {
-		if (!(request instanceof MultipartHttpServletRequest)) {
-			return new BaseState(false, AppInfo.NOT_MULTIPART_CONTENT);
-		}
-		InputStream is = null;
-		try {
-			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-			String fieldName = conf.getFieldName();
-			MultipartFile file = multipartRequest.getFile(fieldName);
+        if (!(request instanceof MultipartHttpServletRequest)) {
+            return new BaseState(false, AppInfo.NOT_MULTIPART_CONTENT);
+        }
+        InputStream is = null;
+        try {
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            String fieldName = conf.getFieldName();
+            MultipartFile file = multipartRequest.getFile(fieldName);
 
-			String savePath = conf.getSavePath();
-			String originFileName = file.getOriginalFilename();
-			String suffix = FileType.getSuffixByFilename(originFileName);
+            String savePath = conf.getSavePath();
+            String originFileName = file.getOriginalFilename();
+            String suffix = FileType.getSuffixByFilename(originFileName);
 
-			originFileName = originFileName.substring(0, originFileName.length() - suffix.length());
-			savePath = savePath + suffix;
+            originFileName = originFileName.substring(0, originFileName.length() - suffix.length());
+            savePath = savePath + suffix;
 
-			long maxSize = conf.getMaxSize();
+            long maxSize = conf.getMaxSize();
+            //使用文件头判断文件类型
+            if (!validType("." + com.gt.ms.ueditor.util.FileType.getFileType(file.getBytes()), conf.getAllowFiles())) {
+                return new BaseState(false, AppInfo.NOT_ALLOW_FILE_TYPE);
+            }
+//            if (!validType(suffix, conf.getAllowFiles())) {
+//                return new BaseState(false, AppInfo.NOT_ALLOW_FILE_TYPE);
+//            }
 
-			if (!validType(suffix, conf.getAllowFiles())) {
-				return new BaseState(false, AppInfo.NOT_ALLOW_FILE_TYPE);
-			}
+            savePath = PathFormat.parse(savePath, originFileName);
+            String rootPath = conf.getRootPath();
 
-			savePath = PathFormat.parse(savePath, originFileName);
-			String rootPath = conf.getRootPath();
+            is = file.getInputStream();
+            State storageState = fileManager.saveFile(is, rootPath, savePath, maxSize);
 
-			is = file.getInputStream();
-			State storageState = fileManager.saveFile(is, rootPath, savePath, maxSize);
+            if (storageState.isSuccess()) {
+                String returnPath = PathFormat.format(savePath);
+                // -------- 判断是否加密 twt
+                Boolean aBoolean = EncAndDecConstants.encrypt.get();
+                if (aBoolean != null && aBoolean) {//加密
+                    returnPath = EncAndDecConstants.FOLDER_BEFORE + returnPath;//加密文件加文件夹前缀
+                }
+                // -------- 判断是否加密 twt
+                storageState.putInfo("url", returnPath);
+                storageState.putInfo("type", suffix);
+                storageState.putInfo("original", originFileName + suffix);
+            }
+            return storageState;
+        } catch (IOException e) {
+            return new BaseState(false, AppInfo.IO_ERROR);
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
 
-			if (storageState.isSuccess()) {
-				String returnPath  = PathFormat.format(savePath);
-                returnPath ="/uploadpic"+ returnPath.substring(returnPath.indexOf("/",2));//返回文件路径时，将首个文件夹名替换成uploadpic。
-				storageState.putInfo("url", returnPath);
-				storageState.putInfo("type", suffix);
-				storageState.putInfo("original", originFileName + suffix);
-			}
-			return storageState;
-		} catch (IOException e) {
-			return new BaseState(false, AppInfo.IO_ERROR);
-		} finally {
-			IOUtils.closeQuietly(is);
-		}
+    }
 
-	}
-
-	private static boolean validType(String type, List<String> allowTypes) {
-		return allowTypes.contains(type);
-	}
+    private static boolean validType(String type, List<String> allowTypes) {
+        return allowTypes.contains(type);
+    }
 }
